7c104529aae956d6438b056fae0c03be191fa39c
CARDOSO Olivier <olivier.al.cardoso@gmail.com>
2018-06-18 22:01:45 +0200

    Driver

---
+++ src/main/generated-sources/sarl/org/IA51/Traffic_sim/Traffic_simulation/agents/DriveWithoutConstraint.java
@@ -0,0 +1,18 @@
+package org.IA51.Traffic_sim.Traffic_simulation.agents;
+
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+
+/**
+ * @author User
+ */
+@SarlSpecification("0.7")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class DriveWithoutConstraint {
+  @SyntheticMember
+  public DriveWithoutConstraint() {
+    super();
+  }
+}
---
+++ src/main/generated-sources/sarl/org/IA51/Traffic_sim/Traffic_simulation/agents/Driver.java
@@ -0,0 +1,105 @@
+package org.IA51.Traffic_sim.Traffic_simulation.agents;
+
+import io.sarl.core.Initialize;
+import io.sarl.core.Lifecycle;
+import io.sarl.lang.annotation.ImportedCapacityFeature;
+import io.sarl.lang.annotation.PerceptGuardEvaluator;
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import io.sarl.lang.core.Agent;
+import io.sarl.lang.core.BuiltinCapacitiesProvider;
+import io.sarl.lang.core.DynamicSkillProvider;
+import io.sarl.lang.core.Skill;
+import io.sarl.lang.util.ClearableReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+import javax.inject.Inject;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Inline;
+import org.eclipse.xtext.xbase.lib.Pure;
+
+@SarlSpecification("0.7")
+@SarlElementType(18)
+@SuppressWarnings("all")
+public class Driver extends Agent {
+  private /* ArrayList<EnvironmentObjecAbstract> */Object pecepts;
+  
+  @SyntheticMember
+  private void $behaviorUnit$Initialize$0(final Initialize occurrence) {
+  }
+  
+  @SyntheticMember
+  private void $behaviorUnit$void$1(final /* CarAccident */Object occurrence) {
+    Lifecycle _$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE$CALLER = this.$castSkill(Lifecycle.class, (this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE == null || this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE.get() == null) ? (this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE = this.$getSkill(Lifecycle.class)) : this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE);
+    _$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE$CALLER.killMe();
+  }
+  
+  @SyntheticMember
+  private void $behaviorUnit$void$2(final /* Perception */Object occurrence) {
+  }
+  
+  @Extension
+  @ImportedCapacityFeature(Lifecycle.class)
+  @SyntheticMember
+  private transient ClearableReference<Skill> $CAPACITY_USE$IO_SARL_CORE_LIFECYCLE;
+  
+  @SyntheticMember
+  @Pure
+  @Inline(value = "$castSkill(Lifecycle.class, ($0$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE == null || $0$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE.get() == null) ? ($0$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE = $0$getSkill(Lifecycle.class)) : $0$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE)", imported = Lifecycle.class)
+  private Lifecycle $CAPACITY_USE$IO_SARL_CORE_LIFECYCLE$CALLER() {
+    if (this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE == null || this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE.get() == null) {
+      this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE = $getSkill(Lifecycle.class);
+    }
+    return $castSkill(Lifecycle.class, this.$CAPACITY_USE$IO_SARL_CORE_LIFECYCLE);
+  }
+  
+  @SyntheticMember
+  @PerceptGuardEvaluator
+  private void $guardEvaluator$Initialize(final Initialize occurrence, final Collection<Runnable> ___SARLlocal_runnableCollection) {
+    assert occurrence != null;
+    assert ___SARLlocal_runnableCollection != null;
+    ___SARLlocal_runnableCollection.add(() -> $behaviorUnit$Initialize$0(occurrence));
+  }
+  
+  @SyntheticMember
+  @PerceptGuardEvaluator
+  private void $guardEvaluator$void(final /* CarAccident */Object occurrence, final Collection<Runnable> ___SARLlocal_runnableCollection) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nCarAccident cannot be resolved to a type.");
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public boolean equals(final Object obj) {
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    return result;
+  }
+  
+  @SyntheticMember
+  public Driver(final UUID parentID, final UUID agentID) {
+    super(parentID, agentID);
+  }
+  
+  @SyntheticMember
+  @Inject
+  @Deprecated
+  public Driver(final BuiltinCapacitiesProvider provider, final UUID parentID, final UUID agentID) {
+    super(provider, parentID, agentID);
+  }
+  
+  @SyntheticMember
+  @Inject
+  public Driver(final UUID parentID, final UUID agentID, final DynamicSkillProvider skillProvider) {
+    super(parentID, agentID, skillProvider);
+  }
+}
