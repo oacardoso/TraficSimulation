3bc8d2913e857bf84419174d9e63dda847d1566e
Unknown <mignot.matthieu90@gmail.com>
2018-06-19 16:03:27 +0200

    DriveWithoutConstraint

--- src/main/generated-sources/sarl/agents/DriveWithoutConstraint.java
+++ src/main/generated-sources/sarl/agents/DriveWithoutConstraint.java
@@ -3,6 +3,7 @@ package agents;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
+import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
  * @author Olivier
@@ -11,6 +12,27 @@ import io.sarl.lang.annotation.SyntheticMember;
 @SarlElementType(10)
 @SuppressWarnings("all")
 public class DriveWithoutConstraint {
+  @Pure
+  public float run(final int maxacc, final int currentacc, final int currentspeed, final int speedlimit) {
+    float acc = 0;
+    int accel = 0;
+    if ((currentspeed > speedlimit)) {
+      acc = (currentacc * (speedlimit / currentspeed));
+    } else {
+      if ((currentspeed == speedlimit)) {
+        acc = currentacc;
+      } else {
+        accel = (currentacc * (speedlimit / currentspeed));
+      }
+    }
+    if ((accel > maxacc)) {
+      acc = maxacc;
+    } else {
+      acc = accel;
+    }
+    return acc;
+  }
+  
   @SyntheticMember
   public DriveWithoutConstraint() {
     super();
