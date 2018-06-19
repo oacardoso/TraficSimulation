938c0bee73daa5ea3efe5865278ae5d6b8618aed
CARDOSO Olivier <olivier.al.cardoso@gmail.com>
2018-06-19 15:58:33 +0200

    Body Of driver : car

---
+++ src/main/generated-sources/sarl/environnement/Car.java
@@ -0,0 +1,19 @@
+package environnement;
+
+import environnement.EnvironmentObjecAbstractMob;
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+
+/**
+ * @author User
+ */
+@SarlSpecification("0.7")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class Car extends EnvironmentObjecAbstractMob {
+  @SyntheticMember
+  public Car() {
+    super();
+  }
+}
