986e73374392bea95d55de3489839f9abdf7eabf
CARDOSO Olivier <olivier.al.cardoso@gmail.com>
2018-06-20 15:58:59 +0200

    Add file interface

--- src/main/generated-sources/sarl/environnement/Car.java
+++ src/main/generated-sources/sarl/environnement/Car.java
@@ -1,47 +1,13 @@
 package environnement;
 
-import environnement.EnvironmentObjecAbstractMob;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
-import io.sarl.lang.annotation.SyntheticMember;
-import javafx.scene.paint.Color;
-import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
  * @author User
  */
 @SarlSpecification("0.7")
-@SarlElementType(10)
+@SarlElementType(11)
 @SuppressWarnings("all")
-public class Car extends EnvironmentObjecAbstractMob {
-  private Color color;
-  
-  @Pure
-  public Color get_Color() {
-    return this.color;
-  }
-  
-  public void set_Color(final Color x) {
-    this.color = x;
-  }
-  
-  @Override
-  @Pure
-  @SyntheticMember
-  public boolean equals(final Object obj) {
-    return super.equals(obj);
-  }
-  
-  @Override
-  @Pure
-  @SyntheticMember
-  public int hashCode() {
-    int result = super.hashCode();
-    return result;
-  }
-  
-  @SyntheticMember
-  public Car() {
-    super();
-  }
+public interface Car {
 }
--- src/main/generated-sources/sarl/environnement/CarAgent.java
+++ src/main/generated-sources/sarl/environnement/CarAgent.java
@@ -5,6 +5,8 @@ import environnement.EnvironmentObjecAbstractMob;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
+import javafx.scene.paint.Color;
+import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
  * @author User
@@ -13,6 +15,32 @@ import io.sarl.lang.annotation.SyntheticMember;
 @SarlElementType(10)
 @SuppressWarnings("all")
 public class CarAgent extends EnvironmentObjecAbstractMob implements Car {
+  private Color color;
+  
+  @Pure
+  public Color get_Color() {
+    return this.color;
+  }
+  
+  public void set_Color(final Color x) {
+    this.color = x;
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public boolean equals(final Object obj) {
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    return result;
+  }
+  
   @SyntheticMember
   public CarAgent() {
     super();
