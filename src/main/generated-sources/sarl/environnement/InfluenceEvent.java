d73a534ed1eb4357be3dbc7d63bf40e155689131
CARDOSO Olivier <olivier.al.cardoso@gmail.com>
2018-06-20 16:13:38 +0200

    influence event

--- src/main/generated-sources/sarl/environnement/InfluenceEvent.java
+++ src/main/generated-sources/sarl/environnement/InfluenceEvent.java
@@ -1,25 +1,50 @@
 package environnement;
 
+import environnement.Influence;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
-import io.sarl.lang.core.Address;
 import io.sarl.lang.core.Event;
+import java.util.List;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Pure;
+import org.eclipse.xtext.xbase.lib.util.ToStringBuilder;
 
 @SarlSpecification("0.7")
 @SarlElementType(15)
 @SuppressWarnings("all")
 public class InfluenceEvent extends Event {
+  public final List<Influence> influences;
+  
+  public InfluenceEvent(final Influence... e) {
+    this.influences = ((List<Influence>)Conversions.doWrapArray(e));
+  }
+  
+  @Override
+  @Pure
   @SyntheticMember
-  public InfluenceEvent() {
-    super();
+  public boolean equals(final Object obj) {
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    return result;
+  }
+  
+  /**
+   * Returns a String representation of the InfluenceEvent event's attributes only.
+   */
+  @SyntheticMember
+  @Pure
+  protected void toString(final ToStringBuilder builder) {
+    super.toString(builder);
+    builder.add("influences", this.influences);
   }
   
   @SyntheticMember
-  public InfluenceEvent(final Address source) {
-    super(source);
-  }
-  
-  @SyntheticMember
-  private final static long serialVersionUID = 588368462L;
+  private final static long serialVersionUID = 3217037845L;
 }
